b0eca56e2db200f744498d7e16bef07b066bbb91
Xueqa <32300578+Xueqa@users.noreply.github.com>
2020-04-09 15:16:10 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -13,39 +13,49 @@ var static=require('koa-static');
 var bodyparser=require('koa-bodyparser');
 var render=require('koa-art-template');
 var path = require('path');
-var username;
-
+var mysql= require('koa-mysql');
+var DB = require('./modules/Database.js');
+var result=[];
 router.get('/',async (ctx,next)=>{
     await ctx.render("index");
+    next();
+
 });
 
 router.post('/login', async (ctx,next)=>{
-    let userinfo=ctx.request.body;
-    username=userinfo.username;
-    if (username==""){
-        ctx.redirect('/');
-        return false;
-    }
-
     ctx.redirect('/main');
+    next();
 });
 
 router.get('/main', async (ctx,next)=>{
-    await ctx.render("main_page" ,{
-        username:username
-    });
+    await ctx.render("main_page");
+    next();
 });
 router.get('/link1',async (ctx,next)=>{
-    await ctx.render('Link1');
+    var aisle= await DB.find('SELECT DISTINCT Aisle From table1');
+    var department= await DB.find('SELECT DISTINCT Department From table1');
+    await ctx.render('Link1',{
+        list:result,
+        aisle:aisle,
+        department:department
+    });
+    next();
+});
+router.post('/link1',async (ctx,next)=>{
+    var param=ctx.request.body;
+    result= await DB.find('SELECT * FROM table1 WHERE Aisle=? AND Department=?',[param.aisle,param.department]);
+    console.log(result);
+    await ctx.redirect('/link1');
 })
+
+
 render(app, {
     root: path.join(__dirname, 'views'),
     extname: '.html',
     debug: process.env.NODE_ENV !== 'production'
 });
-
 app.use(bodyparser());
-app.use(static("CSS"));
+app.use(static(path.join(__dirname,'css')));
 app.use(views("views",{
     map: {
         html: 'ejs'
@@ -57,3 +67,4 @@ app.use(router.allowedMethods());
 app.listen(3000,()=> {
     console.log("starting at port 3000");
 });
+module.exports = app;
